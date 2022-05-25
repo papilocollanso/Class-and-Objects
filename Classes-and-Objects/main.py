9da251d9258c6875b93cb79fc848416862bbd7b0
Collins Prosper <papilocollanso@gmail.com>
2022-05-25 19:54:33 +0100

    Update main.py

--- Classes-and-Objects/main.py
+++ Classes-and-Objects/main.py
@@ -1,7 +1,23 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self):
-        pass
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.score = float(score)
+        self.tracks = tracks
+    def change_name(self, name):
+         self.name = name
+         return name
+    
+    def change_age(self, age):
+         self.age = int(age)
+         return age
+    def add_track(self, tracks):
+         self.tracks.append(tracks)
+         return tracks
+         
+    def get_score(self):
+        return self.score
 
 
 
@@ -12,3 +28,4 @@ Bob.change_name("Peter")
 Bob.change_age(34)
 Bob.add_track("UI/UX")
 Bob.get_score()
+
